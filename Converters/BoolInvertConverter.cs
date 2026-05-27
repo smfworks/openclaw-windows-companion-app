a9be2ced9657813eae28cc5cae8e08de1a0a1a7e
Michael Gannotti <michael@smfworks.com>
2026-05-27 16:44:47 -0400

    Fix: Edit button in config panel not working
    - Fixed BoolInvertConverter to properly return Visibility values
    - Changed Edit button from Command binding to Click event handler
    - Click handler directly sets IsConfigEditing = true on the ViewModel

--- Converters/BoolInvertConverter.cs
+++ Converters/BoolInvertConverter.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace OpenClawCompanion.Converters;
@@ -8,14 +9,22 @@ public class BoolInvertConverter : IValueConverter
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (value is bool b)
-            return !b;
-        return false;
+        {
+            var inverted = !b;
+            // If target is Visibility, return the proper enum value
+            if (targetType == typeof(Visibility))
+                return inverted ? Visibility.Visible : Visibility.Collapsed;
+            return inverted;
+        }
+        return targetType == typeof(Visibility) ? Visibility.Collapsed : false;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (value is bool b)
             return !b;
+        if (value is Visibility v)
+            return v != Visibility.Visible;
         return false;
     }
 }
